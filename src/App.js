0520e9c0f146e4827bb96ae0231ef0aecae4cb37
Jovincent Del Mundo <jovincent.delmundo@gmail.com>
2021-09-20 20:47:05 -0700

    checking app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React, {Component} from 'react';
+// import axios from 'axios';
 // import logo from './logo.svg';
 // import './App.css';
-import React, {Component} from 'react';
-import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
